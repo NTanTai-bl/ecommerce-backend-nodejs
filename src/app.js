94c6412828bc0d16c23f9989936db30f6bec9b60
Tuan Anh <tuananh.phung@dssolution.jp>
2026-01-04 01:51:52 +0700

    TANTAI-2: Node.js Backend Architecture - Những folders và packages cần thiết khi khởi tạo Project!

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
+
+// init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+// init database
+// init routes
+
+module.exports = app;
