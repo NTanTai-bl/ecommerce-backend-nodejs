3db99a21f2bf8d6f31baa3b350c023a48786da5a
Tuan Anh <tuananh.phung@dssolution.jp>
2026-01-19 22:30:56 +0700

    TANTAI-7: Section 7: Xử lý ErrorHandler trong API

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,14 +1,8 @@
 const AccessService = require("../services/access.service");
 
 class AccessController {
-  async signUp(req, res, next) {
-    try {
-      console.log(`[P]::signUp::`, req.body);
-
+  signUp = async(req, res, next) => {
       return res.status(201).json(await AccessService.singUp(req.body));
-    } catch (err) {
-      next(err);
-    }
   }
 }
 
